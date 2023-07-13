e72eb821845a57aeff8129f6c615f93fb725bbef
yuchao1013 <yuchao1013@gmail.com>
2023-07-13 22:50:55 +0800

    Initial commit

---
+++ ToolsForCostEngineer-backend/src/main/java/com/yuchao/toolsforcostengineerbackend/ToolsForCostEngineerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yuchao.toolsforcostengineerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsForCostEngineerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsForCostEngineerBackendApplication.class, args);
+    }
+
+}
---
+++ ToolsForCostEngineer-backend/src/test/java/com/yuchao/toolsforcostengineerbackend/ToolsForCostEngineerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuchao.toolsforcostengineerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsForCostEngineerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
